d6601986e512598a2d19be2683417ae38a09600f
Diptis Halder <diptis.halder@gmail.com>
2019-02-21 18:58:08 +0000

    v1.2 Created NoteListView class and method to produce a <div> containing a <ul> in which each <li> contains a NoteList item

---
+++ spec/note-list-view-spec.js
@@ -0,0 +1,16 @@
+// note-list-view-spec.js
+
+'use-strict';
+
+function testContentContainsListInHTML() {
+  var list1 = new NoteList();
+  var listview1 = new NoteListView();
+  list1.createNoteAndAddToList("Blue Moon");
+  list1.createNoteAndAddToList("You saw me standing alone");
+  list1.createNoteAndAddToList("Without a dream in my heart");
+  list1.createNoteAndAddToList("Without a love of my own");
+  assert.isTrue(listview1.showNotesInListInHTML(list1) === 
+    "<div><ul><li>Blue Moon</li><li>You saw me standing alone</li><li>Without a dream in my heart</li><li>Without a love of my own</li></ul></div>");
+  console.log("testContentContainsListInHTML did run");
+}
+testContentContainsListInHTML();
---
+++ src/note-list-view.js
@@ -0,0 +1,22 @@
+// note-list-view.js
+'use strict';
+
+(function (exports) {
+
+  function NoteListView() {
+    this._notelistview = "";
+  };
+  
+  NoteListView.prototype.showNotesInListInHTML = function(list) {
+    var listItems = "";
+    for (var i=0; i < list._notelist.length; i++) {
+      listItems += "<li>" + list._notelist[i]._text + "</li>";
+      console.log("listItems:");
+      console.log(listItems);
+    }
+    this._notelistview = "<div><ul>" + listItems + "</ul></div>";
+    return this._notelistview;
+  };
+
+  exports.NoteListView = NoteListView;
+})(this);
